d0df78baddaef3b06a9a834dbbcd69ccc115ee68
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2015-07-31 15:21:33 +0200

    add dependencies for tests

---
+++ tests/testing_settings.py
@@ -0,0 +1,62 @@
+#URI for postgresql
+# postgresql://<user>:<password>@<host>:<port>/<dbname>
+#http://docs.sqlalchemy.org/en/rel_0_9/dialects/postgresql.html#psycopg2
+SQLALCHEMY_DATABASE_URI = 'postgresql://navitia:navitia@localhost/kirin'
+
+DEBUG = True
+
+#rabbitmq connections string: http://kombu.readthedocs.org/en/latest/userguide/connections.html#urls
+RABBITMQ_CONNECTION_STRING='pyamqp://guest:guest@localhost:5672//?heartbeat=60'
+
+#amqp exhange used for sending disruptions
+EXCHANGE='navitia'
+
+ENABLE_RABBITMQ=True
+
+#Log Level available
+# - DEBUG
+# - INFO
+# - WARN
+# - ERROR
+
+# logger configuration
+LOGGER = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters':{
+        'default': {
+            'format': '[%(asctime)s] [%(levelname)5s] [%(process)5s] [%(name)25s] %(message)s',
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+        },
+        'amqp':{
+            'level': 'DEBUG',
+        },
+        'sqlalchemy.engine': {
+            'handlers': ['default'],
+            'level': 'WARN',
+            'propagate': False
+        },
+        'sqlalchemy.pool': {
+            'handlers': ['default'],
+            'level': 'WARN',
+            'propagate': False
+        },
+        'sqlalchemy.dialects.postgresql': {
+            'handlers': ['default'],
+            'level': 'WARN',
+            'propagate': False
+        },
+    }
+}
