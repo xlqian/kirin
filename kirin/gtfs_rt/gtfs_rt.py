9c55642f6c436444292ca877a1fa957de67f1370
Pierre-Etienne Bougue <pierre-etienne.bougue@canaltp.fr>
2017-09-04 14:49:51 +0200

    empty boxes for gtfs-rt

---
+++ kirin/gtfs_rt/gtfs_rt.py
@@ -0,0 +1,78 @@
+# Copyright (c) 2001-2017, Canal TP and/or its affiliates. All rights reserved.
+#
+# This file is part of Navitia,
+#     the software to build cool stuff with public transport.
+#
+# Hope you'll enjoy and contribute to this project,
+#     powered by Canal TP (www.canaltp.fr).
+# Help us simplify mobility and open public transport:
+#     a non ending quest to the responsive locomotion way of traveling!
+#
+# LICENCE: This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Stay tuned using
+# twitter @navitia
+# IRC #navitia on freenode
+# https://groups.google.com/d/forum/navitia
+# www.navitia.io
+import flask
+from flask.globals import current_app
+from flask_restful import Resource
+from kirin import core
+from kirin.core import model
+from kirin.exceptions import KirinException
+
+
+def _get_gtfs_rt(request):
+    pass
+
+
+def _make_rt_update(raw_proto):
+    pass
+
+
+def make_model(rt_update):
+    pass
+
+
+class GtfsRT(Resource):
+
+    def __init__(self):
+        pass
+
+    def post(self):
+
+        raw_proto = _get_gtfs_rt(flask.globals.request)
+
+        # create a raw ire obj, save the raw_xml into the db
+        rt_update = _make_rt_update(raw_proto)
+        try:
+            # raw_xml is interpreted
+            trip_updates = make_model(rt_update)
+        except KirinException as e:
+            rt_update.status = 'KO'
+            rt_update.error = e.data['error']
+            model.db.session.add(rt_update)
+            model.db.session.commit()
+            raise
+        except Exception as e:
+            rt_update.status = 'KO'
+            rt_update.error = e.message
+            model.db.session.add(rt_update)
+            model.db.session.commit()
+            raise
+
+        core.handle(rt_update, trip_updates, current_app.config['CONTRIBUTOR'])
+
+        return 'OK', 200
