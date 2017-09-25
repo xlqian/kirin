d2eb6e666bc3b258a19780aeee0e3ce795d55c9d
xlqian <patrick.qian@canaltp.fr>
2017-09-25 15:35:35 +0200

    optimization after rebase

--- kirin/gtfs_rt/model_maker.py
+++ kirin/gtfs_rt/model_maker.py
@@ -148,37 +148,39 @@ class KirinModelBuilder(object):
             record_internal_failure('gtfs-rt', 'duplicate vjs')
             return []
        
-        vjs = []
-        for nav_vj in navitia_vjs:
-            # Now we compute the real circulate_date of VJ from since, until, data_time and vj's first stop_time
-            # We do this to prevent cases like pass midnight when [since, until] is too large
-            # the final circulate_date in database is in local timezone
-            first_stop_time = nav_vj.get('stop_times', [{}])[0]
-            tzinfo = get_timezone(first_stop_time)
+        nav_vj = navitia_vjs[0]
 
-            # 'since' and 'until' must have a timezone before being converted to local timezone
-            local_since = pytz.utc.localize(since).astimezone(tzinfo)
-            local_until = pytz.utc.localize(until).astimezone(tzinfo)
+        # Now we compute the real circulate_date of VJ from since, until, data_time and vj's first stop_time
+        # We do this to prevent cases like pass midnight when [since, until] is too large
+        # the final circulate_date in database is in local timezone
+        first_stop_time = nav_vj.get('stop_times', [{}])[0]
+        tzinfo = get_timezone(first_stop_time)
 
-            circulate_date = None
+        # 'since' and 'until' must have a timezone before being converted to local timezone
+        local_since = pytz.utc.localize(since).astimezone(tzinfo)
+        local_until = pytz.utc.localize(until).astimezone(tzinfo)
 
-            if local_since.date() == local_until.date():
+        circulate_date = None
+
+        if local_since.date() == local_until.date():
+            circulate_date = local_since.date()
+        else:
+            arrival_time = first_stop_time['arrival_time']
+            # At first, we suppose that the circulate_date is local_since's date
+            if local_since < tzinfo.localize(datetime.datetime.combine(local_since.date(),
+                                                                       arrival_time)) < local_until:
                 circulate_date = local_since.date()
+            elif local_since < tzinfo.localize(datetime.datetime.combine(local_until.date(),
+                                                                         arrival_time)) < local_until:
+                circulate_date = local_until.date()
             else:
-                arrival_time = first_stop_time['arrival_time']
-                # At first, we suppose that the circulate_date is local_since's date
-                if local_since < tzinfo.localize(datetime.datetime.combine(local_since.date(),
-                                                                           arrival_time)) < local_until:
-                    circulate_date = local_since.date()
-                elif local_since < tzinfo.localize(datetime.datetime.combine(local_until.date(),
-                                                                             arrival_time)) < local_until:
-                    circulate_date = local_until.date()
-                else:
-                    self.log.error('impossible to calculate the circulate date of vj: {}'.format(nav_vj.get('id')))
+                self.log.error('impossible to calculate the circulate date of vj: {}'.format(nav_vj.get('id')))
 
-            if circulate_date:
-                vjs.append(model.VehicleJourney(nav_vj, circulate_date))
-        return vjs
+        if not circulate_date:
+            return []
+
+        return [model.VehicleJourney(nav_vj, circulate_date)]
+
 
     def _make_stoptime_update(self, input_st_update, navitia_vj):
         nav_st = self._get_navitia_stop_time(input_st_update, navitia_vj)
