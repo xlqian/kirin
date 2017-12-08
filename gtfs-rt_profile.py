8439987dc0e2e6461c120ec86bf1e047c87b6cac
XL <patrick.qian@canaltp.fr>
2017-12-08 10:29:38 +0100

    add description

--- gtfs-rt_profile.py
+++ gtfs-rt_profile.py
@@ -1,3 +1,31 @@
+# Copyright (c) 2001-2015, Canal TP and/or its affiliates. All rights reserved.
+#
+# This file is part of Navitia,
+#     the software to build cool stuff with public transport.
+#
+#     powered by Canal TP (www.canaltp.fr).
+# Help us simplify mobility and open public transport:
+#     a non ending quest to the responsive locomotion way of traveling!
+#
+# LICENCE: This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Stay tuned using
+# twitter @navitia
+# IRC #navitia on freenode
+# https://groups.google.com/d/forum/navitia
+# www.navitia.io
+
 from kirin import gtfs_realtime_pb2
 import navitia_wrapper
 from kirin.gtfs_rt import model_maker
@@ -46,10 +74,21 @@ to review your profile result
 
 """
 
+description_string = """
+This tool is used to profile the processing of gtfs-rt
+
+To run the profiler:
+
+PYTHONPATH=. KIRIN_CONFIG_FILE=dev_settings.py 
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2 
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp 
+python gtfs-rt_profile.py --proto 'tripUpdates.pb' --dump test.prf
+"""
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=description_string)
     parser.add_argument('--proto', nargs='?', help='proto file')
-    parser.add_argument('--dump', default='dumped_profile.prf', nargs='?', help='dumped profiling file')
+    parser.add_argument('--dump', default='dumped_profile.prf', nargs='?', help='where to dump profiling file')
 
     args = parser.parse_args()
 
