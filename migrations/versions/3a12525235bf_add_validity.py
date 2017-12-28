da9add4c235dce427b6053ea45a4121d1f0f4b6c
kadhikari <krishna.adhikari@canaltp.fr>
2017-12-28 15:49:22 +0100

    attribut validity added in the table real_time_update

---
+++ migrations/versions/3a12525235bf_add_validity.py
@@ -0,0 +1,25 @@
+"""
+Add an attribut validity of type boolean which will contain the validity of inserted gtfs-rt
+
+Revision ID: 3a12525235bf
+Revises: 2763a0e3f0bf
+Create Date: 2017-12-28 14:56:25.238546
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3a12525235bf'
+down_revision = '2763a0e3f0bf'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('real_time_update', sa.Column('validity', sa.Boolean(), nullable=True))
+    op.execute("UPDATE real_time_update SET validity = True WHERE validity IS NULL")
+    op.alter_column('real_time_update', 'validity', nullable=False)
+
+
+def downgrade():
+    op.drop_column('real_time_update', 'validity')
