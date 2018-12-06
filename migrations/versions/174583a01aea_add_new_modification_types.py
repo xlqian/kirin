9879cdd9d2a409b9efa34add6917d5ef0f74ec5b
Nonifier <olivier.georget@kisio.org>
2018-12-06 10:17:46 +0100

    Fix alembic migration script

--- migrations/versions/174583a01aea_add_new_modification_types.py
+++ migrations/versions/174583a01aea_add_new_modification_types.py
@@ -11,8 +11,10 @@ revision = '174583a01aea'
 down_revision = '4d9df787c7a7'
 
 from alembic import op
+import sqlalchemy as sa
 
 def upgrade():
+    op.execute("COMMIT")
     op.execute("ALTER type modification_type ADD VALUE 'deleted_for_detour'")
     op.execute("ALTER type modification_type ADD VALUE 'added_for_detour'")
 
@@ -20,4 +22,25 @@ def downgrade():
     """
     Postgresql doesn't support the removal of a specific enum value
     """
-    pass
+    op.execute("ALTER TABLE stop_time_update ALTER COLUMN departure_status TYPE text")
+    op.execute("ALTER TABLE stop_time_update ALTER COLUMN arrival_status TYPE text")
+    op.execute("ALTER TABLE trip_update ALTER COLUMN status TYPE text")
+
+    op.execute("UPDATE stop_time_update SET arrival_status='add' WHERE arrival_status='added_for_detour'")
+    op.execute("UPDATE stop_time_update SET arrival_status='delete' WHERE arrival_status='deleted_for_detour'")
+
+    op.execute("UPDATE stop_time_update SET departure_status='add' WHERE departure_status='added_for_detour'")
+    op.execute("UPDATE stop_time_update SET departure_status='delete' WHERE departure_status='deleted_for_detour'")
+
+    op.execute("UPDATE trip_update SET status='add' WHERE status='added_for_detour'")
+    op.execute("UPDATE trip_update SET status='delete' WHERE status='deleted_for_detour'")
+
+    sa.Enum('', name='modification_type').drop(op.get_bind())
+
+    op.execute("CREATE TYPE modification_type AS ENUM ('add', 'delete', 'update', 'none')")
+
+    op.execute("ALTER TABLE stop_time_update ALTER COLUMN departure_status TYPE modification_type USING departure_status::modification_type")
+    op.execute("ALTER TABLE stop_time_update ALTER COLUMN arrival_status TYPE modification_type USING arrival_status::modification_type")
+    op.execute("ALTER TABLE trip_update ALTER COLUMN status TYPE modification_type USING status::modification_type")
+
+
