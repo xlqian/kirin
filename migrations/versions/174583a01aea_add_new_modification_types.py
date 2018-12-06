b5d5d56b62e71183250493f5f65dfec34622697e
Nonifier <olivier.georget@kisio.org>
2018-12-06 11:10:18 +0100

    simplify alembic merge

--- migrations/versions/174583a01aea_add_new_modification_types.py
+++ migrations/versions/174583a01aea_add_new_modification_types.py
@@ -8,7 +8,7 @@ Create Date: 2018-11-28 14:58:50.362398
 
 # revision identifiers, used by Alembic.
 revision = '174583a01aea'
-down_revision = '4d9df787c7a7'
+down_revision = '45f4c90aa775'
 
 from alembic import op
 import sqlalchemy as sa
